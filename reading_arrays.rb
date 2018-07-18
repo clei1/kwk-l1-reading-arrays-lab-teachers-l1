1afc84dbb44c031523352a0107ee91f32bb44f4a
clei1 <callmecon328@gmail.com>
2018-07-18 23:19:40 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -19,11 +19,12 @@ end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-  return STUDENT_NAMES[STUDENT_NAMES.size -1]
+  return STUDENT_NAMES[STUDENT_NAMES.size - 1]
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  return STUDENT_NAMES.first
 end
 
 def last_student_by_method
